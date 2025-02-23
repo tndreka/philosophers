b41eff1768f4df07bc9590175442dc27648a0c7c
tndreka <tndreka@student.42heilbronn.de>
2025-02-23 20:30:22 +0100

    added the optional_meal -> debuging bc they eat more than they should

--- src/dining/monitor.c
+++ src/dining/monitor.c
@@ -6,7 +6,7 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 16:07:14 by tndreka           #+#    #+#             */
-/*   Updated: 2025/02/23 20:08:14 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/02/23 20:28:50 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,19 @@ void	*philo_camera(void *arg)
 		{
 			if (check_philo_dead(dining, i))
 				return (NULL);
+			pthread_mutex_lock(&dining->meal_lock);
+			if (dining->meal_flag != -1 && dining->philos[i].meal_count >= dining->meal_flag)
+				full++;
+			pthread_mutex_unlock(&dining->meal_lock);
+			i++;
 		}
+		if (dining->meal_flag != -1 && full == dining->philo_nbr)
+		{
+			pthread_mutex_lock(&dining->dead_lock);
+			dining->finish_routine = true;
+			pthread_mutex_unlock(&dining->dead_lock);
+			return (NULL);
+		}	
 		usleep(500);
 	}
 	return (NULL);
